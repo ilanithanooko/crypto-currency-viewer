918007e179ab3d7318fae6121b3a2abdf279d918
ilanit.hanooko <ilanit.hanooko@e.braude.ac.il>
2024-08-27 23:55:59 +0300

    Tue 08-27-24

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -6,9 +6,9 @@ import "datatables.net";
 // Function to format numbers in millions (M) or billions (B)
 const formatMarketCap = (num) => {
   if (num >= 1e9) {
-    return (num / 1e9).toFixed(2) + '<strong>B</strong>'; // Convert to billions
+    return (num / 1e9).toFixed(2) + 'B'; // Convert to billions
   } else if (num >= 1e6) {
-    return (num / 1e6).toFixed(2) + '<strong>M</strong>'; // Convert to millions
+    return (num / 1e6).toFixed(2) + 'M'; // Convert to millions
   }
   return num.toLocaleString(); // Return number with commas for smaller numbers
 };
